f5fd5a7e53aae6e61c9bec578dcfc5081a7f01b6
Jay Kumar Bharti <60088194+jayyadav362@users.noreply.github.com>
2023-01-27 00:49:17 +0530

    latest

--- src/components/App.js
+++ src/components/App.js
@@ -10,33 +10,33 @@ const states = [
         name: "Indore",
         towns: [
           {
-            name: "Mhow",
+            name: "Mhow"
           },
           {
-            name: "Dewas",
-          },
-        ],
+            name: "Dewas"
+          }
+        ]
       },
       {
         name: "Bhopal",
         towns: [
           {
-            name: "Manit",
+            name: "Manit"
           },
           {
-            name: "Berasia",
-          },
-        ],
+            name: "Berasia"
+          }
+        ]
       },
       {
         name: "Gwalior",
         towns: [
           {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
+            name: "Ajaypur"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Jharkhand",
@@ -45,36 +45,36 @@ const states = [
         name: "Dhanbad",
         towns: [
           {
-            name: "IIT(ISM) Dhanbad",
+            name: "IIT(ISM) Dhanbad"
           },
           {
-            name: "Hirapur",
-          },
-        ],
+            name: "Hirapur"
+          }
+        ]
       },
       {
         name: "Wasseypur",
         towns: [
           {
-            name: "Sardar khan's",
+            name: "Sardar khan's"
           },
           {
-            name: "Faizal khan's",
-          },
-        ],
+            name: "Faizal khan's"
+          }
+        ]
       },
       {
         name: "Mirzapur",
         towns: [
           {
-            name: "Kaleen bhaiya's",
+            name: "Kaleen bhaiya's"
           },
           {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
+            name: "Guddu bhaiya's"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Assam",
@@ -83,36 +83,36 @@ const states = [
         name: "Guwhati",
         towns: [
           {
-            name: "Amin",
+            name: "Amin"
           },
           {
-            name: "Jalah",
-          },
-        ],
+            name: "Jalah"
+          }
+        ]
       },
       {
         name: "Jungle1",
         towns: [
           {
-            name: "Tiger found at IIT Guwahati",
+            name: "Tiger found at IIT Guwahati"
           },
           {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
+            name: "Leopard found in IIT Guwahati"
+          }
+        ]
       },
       {
         name: "Tezpur",
         towns: [
           {
-            name: "Dibrugarh",
+            name: "Dibrugarh"
           },
           {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
+            name: "Silchar"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Bihar",
@@ -121,41 +121,84 @@ const states = [
         name: "Patna",
         towns: [
           {
-            name: "Sonpur",
+            name: "Sonpur"
           },
           {
-            name: "Maner",
-          },
-        ],
+            name: "Maner"
+          }
+        ]
       },
       {
         name: "Gaya",
         towns: [
           {
-            name: "Bakraur",
+            name: "Bakraur"
           },
           {
-            name: "Barachatti",
-          },
-        ],
+            name: "Barachatti"
+          }
+        ]
       },
       {
         name: "Darbhanga",
         towns: [
           {
-            name: "Singhwara",
+            name: "Singhwara"
           },
           {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
+            name: "Jale"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 function App() {
-  return <div id="main"></div>;
-}
+  return(
+    <div className="accordion accordion-flush" id="accordionFlushExample">
+      {states.map((data,idx)=>{
+        return(
+          <div key={idx} id={`state${idx}`} className="accordion-item">
+            <h2 className="accordion-header" id={`flush-headingOne${idx}`}>
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapseOne${idx}`} aria-expanded="false" aria-controls="flush-collapseOne">
+                {data.name}
+              </button>
+            </h2>
+            <div id={`flush-collapseOne${idx}`} className="accordion-collapse collapse" aria-labelledby={`flush-headingOne${idx}`} data-bs-parent="#accordionFlushExample">
+              <div className="accordion-body">
+                <div className="accordion accordion-flush" id={`accordian${idx}`}>  
+                  {data.cities.map((c,cidx)=>{
+                    return(
+                      <div className="accordion-item" key={cidx} id={`city${cidx}`}>
+                      <h2 className="accordion-header" id={`flush-headingTwo${cidx}`}>
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapseTwo${cidx}`} aria-expanded="false" aria-controls={`flush-collapseTwo${cidx}`}>
+                        {c.name}
+                        </button>
+                      </h2>
+                      <div id={`flush-collapseTwo${cidx}`} className="accordion-collapse collapse" aria-labelledby={`flush-headingTwo${cidx}`} data-bs-parent={`#accordian${idx}`}>
+                        <div className="accordion-body">
+                        <ul className="list-group">
+                          {c.towns.map((t,tidx)=>{
+                            return(
+                              <li key={tidx} id={`town${tidx}`} className="list-group-item">
+                                {t.name}
+                              </li>
+                            )
+                          })}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>  
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+  </div>
+  )}
 
 export default App;
